6d29d081c8f63b5914290d3584029f919ceae8da
Alan B <alan@ajbapps.com>
2018-08-22 20:34:12 -0700

    Add protect routes for shows

--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::group(['middleware' => 'auth:api'], function() {
+
+    Route::get('show/', 'ShowController@index')->name('show_list');
+    Route::get('show/{id}', 'ShowController@show')->name('show_detail');
+
+});
